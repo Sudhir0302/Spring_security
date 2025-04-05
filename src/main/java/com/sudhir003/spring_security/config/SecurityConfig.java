ea54eb4b9f9e46566db2678bde493d815ad4e629
Sudhir0302 <141290194+Sudhir0302@users.noreply.github.com.>
2025-04-05 23:50:43 +0530

    implemented jwt

--- src/main/java/com/sudhir003/spring_security/config/SecurityConfig.java
+++ src/main/java/com/sudhir003/spring_security/config/SecurityConfig.java
@@ -1,21 +1,20 @@
-package com.sudhir003.spring_security.security;
+package com.sudhir003.spring_security.config;
 
 import com.sudhir003.spring_security.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -24,6 +23,9 @@ public class SecurityConfig {
 
     @Autowired
     private MyUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Bean
     public AuthenticationProvider authenticationProvider(){
@@ -34,14 +36,22 @@ public class SecurityConfig {
         return provider;
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
     {
         http.csrf(csrf->csrf.disable());
-        http.authorizeHttpRequests(request->request.anyRequest().authenticated());  //here request means all request coming to the backend r
+        http.authorizeHttpRequests(request->request
+                .requestMatchers("register","login")
+                .permitAll()
+                .anyRequest().authenticated());  //here request means all request coming to the backend r
         http.httpBasic(Customizer.withDefaults());  //user login using alert msg
         http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
--- src/main/java/com/sudhir003/spring_security/controller/UserController.java
+++ src/main/java/com/sudhir003/spring_security/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.sudhir003.spring_security.controller;
 
 import com.sudhir003.spring_security.model.User;
+import com.sudhir003.spring_security.service.JwtService;
 import com.sudhir003.spring_security.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +17,31 @@ public class UserController{
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("register")
+    public User adduser(@RequestBody  User user)
+    {
+        userService.saveUser(user);
+        return user;
+    }
+
+    @PostMapping("login")
+    public String login(@RequestBody User user)
+    {
+        //spring security has a in-build userpasswordauthenticationtoken ,which checks username and password and generate token.
+        Authentication authentication=authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(),user.getPassword()));
+        if(authentication.isAuthenticated()){
+            return  jwtService.generateToken(user.getUsername());
+        }
+        return "failed";
+    }
 
     @GetMapping("getuser")
     public ResponseEntity<?> getuser(@RequestParam String username)
@@ -24,10 +53,4 @@ public class UserController{
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
-    @PostMapping("register")
-    public User adduser(@RequestBody  User user)
-    {
-        userService.saveUser(user);
-        return user;
-    }
 }
